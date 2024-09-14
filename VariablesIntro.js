7195aa99c11ddf8377d1dc79ee2a8881defd2a24
khand25 <drk46099@uga.edu>
2024-09-14 10:10:34 -0400

    Followed intro to variables tutorial

--- VariablesIntro.js
+++ VariablesIntro.js
@@ -1,2 +1,59 @@
 // variable = A container that stores a value.
-    //Behaves as if it were the value it contains
+// Behaves as if it were the value it contains
+
+// 1. declaration: let x;
+// 2. assignment: x = 100;
+
+// created a varaible called x using the let keyword
+let x;
+x = 100;
+console.log(x);
+
+// We can also do declaration and assingment of variable in one
+// line
+
+let y = 123;
+console.log("Value of y is: "+y);
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+console.log(gpa);
+
+// format Strings or template literals, can be used in JS by using `` for quotes and
+// ${varaible} for actual varaible values
+
+// typeof statement can be used to check the datatype of
+// the varaible enclosed in parenthesis
+// Remember number is a datatype in JS that includes both int
+// and double values from Java
+console.log(typeof(gpa));
+console.log(`You are ${age} years old.`);
+console.log(`The price is $${price}.`);
+console.log(`Your gpa is: ${gpa}.`);
+
+// Strings
+let firstName = "Danyal Khan";
+let favoriteFood = "Burgers"
+console.log(typeof(firstName));
+console.log(`Your name is: ${firstName}`);
+console.log(`Your like: ${favoriteFood}`);
+
+//Boolean varaibles.
+// declared a varaible called online that is a boolean variable
+let online = true;
+console.log(typeof(online));
+
+let fullName = "Danyal Khan";
+age = 22;
+let isStudent = false;
+
+// Code below overrides all p1, p2, and p3 id's of elements from
+// linked html file with text content on RHS side of varaibles
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `Your age is ${age}`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+
+
+
+
