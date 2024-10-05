dcb430d8ab05f15fcd8dd5dda94a62a15ee11a0a
khand25 <drk46099@uga.edu>
2024-10-05 14:38:02 -0400

    Create PromiesIntro.js

---
+++ PromiesIntro.js
@@ -0,0 +1,5 @@
+// Promise = An object that manages asynchronus operations.
+//      Wrap a Promise Object around (asynchronous code)
+//     "I promise to return a value"
+//     PENDING -> RESOLVED or REJECTED
+//     new Promise((resolve,reject) -> {asynchronous code}) 
